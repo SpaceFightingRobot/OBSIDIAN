90bae914f8125f2026d6d31fe61e9d2ab0545d87
SpaceFightingRobot <Spacefightingrobot@gmail.com>
2013-11-28 23:21:36 +0100

    Basic Classes
    -Added PlayerController  basic vars
    -Added CameraController class - unfilled

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+
+	//Movement//
+
+	private Vector3 PlayerDir;
+	private Vector3 PlayerPos;
+	protected Vector3 PlayerVel;
+	protected Vector3 PlayerRot;
+
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		HandleInput();	
+	}
+
+	
+	void HandleInput()
+	{
+		if(Input.GetKeyDown(KeyCode.W))
+			//Go forward
+		if(Input.GetKeyDown(KeyCode.A))
+			//Go left
+		if(Input.GetKeyDown(KeyCode.S))
+			//Go backward
+		if(Input.GetKeyDown(KeyCode.D));
+			//Go right
+
+		
+	}
+
+
+
+
+}
+

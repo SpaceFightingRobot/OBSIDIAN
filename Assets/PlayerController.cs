a9fb4ac085b35139d76a0360da1df4334c1ab553
SpaceFightingRobot <Spacefightingrobot@gmail.com>
2013-11-29 21:51:53 +0100

    PlayerMovement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,15 +6,25 @@ public class PlayerController : MonoBehaviour {
 
 
 	//Movement//
-
+	public float PlayerGravity = 10f;
+	public float PlayerTurnSpeed = 10f;
+	public float PlayerMaxMoveSpeed = 5f;
+	public float PlayerAcceleration = 1f;
+	public bool IsInAir = false;
+	
 	private Vector3 PlayerPos;
+	private Quaternion PlayerNewRot;
+
+	protected Quaternion PlayerRot;
+	protected float PlayerSpeed;
+	protected Vector3 PlayerVelDir;
 	protected Vector3 PlayerVel;
-	protected Vector3 PlayerRot;
+		
 
 
 
 
-
+ 
 	// Use this for initialization
 	void Start () 
 	{
@@ -26,20 +36,24 @@ public class PlayerController : MonoBehaviour {
 	{	
 		HandleInput();
 		HandlePlayerMovement();
+		//HandleCamera ();
 
 	}
 
 	
 	void HandleInput()
 	{
-		if(Input.GetKeyDown(KeyCode.W))
-			//Go forward
-		if(Input.GetKeyDown(KeyCode.A))
-			//Go left
-		if(Input.GetKeyDown(KeyCode.S))
-			//Go backward
-		if(Input.GetKeyDown(KeyCode.D));
-			//Go right
+		PlayerPos = transform.position;
+		PlayerRot = transform.rotation;
+		//Check player movement
+		if(Mathf.Abs(Input.GetAxis("LeftRight")) > 0 || Mathf.Abs(Input.GetAxis("ForwardBackward")) > 0  )
+			PlayerVelDir = new Vector3(Input.GetAxis("LeftRight"),0,Input.GetAxis("ForwardBackward"));
+
+			if(PlayerSpeed != PlayerMaxMoveSpeed)
+				PlayerSpeed += PlayerAcceleration;
+		//if(Mathf.Abs(Input.GetAxis("Mouse X")) > 0 || Mathf.Abs(Input.GetAxis("Mouse Y")) > 0)
+			//PlayerNewRot = new Quaternion(0,Input.GetAxis("Mouse Y"),0,0) * PlayerRot ;
+
 
 		
 	}
@@ -47,8 +61,20 @@ public class PlayerController : MonoBehaviour {
 
 	void HandlePlayerMovement()
 	{
-		transform.position = PlayerPos + PlayerVel;
-		transform.rotation = PlayerRot;
+
+		PlayerVel = PlayerVelDir * PlayerSpeed;
+
+
+		//DEBUG
+		Debug.DrawLine (PlayerPos, PlayerPos + PlayerVel , Color.red);	
+		//Debug.Log("Mouse" + Input.GetAxis("Mouse X") + "   " + Input.GetAxis("Mouse X");
+
+
+
+		//Set Player Position
+		transform.position = PlayerPos + PlayerVel * Time.deltaTime;
+		//transform.rotation = PlayerNewRot;
+
 
 	}
 

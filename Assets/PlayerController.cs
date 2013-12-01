730bbf4e42b148147efc288db4799c54fc514cb6
SpaceFightingRobot <Spacefightingrobot@gmail.com>
2013-12-01 03:31:30 +0100

    More Refinement
    -Added Player Physics
    -Fixed Camera Rotation
    -Added shitty Jump

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,34 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof(CharacterController))]
 
 public class PlayerController : MonoBehaviour {
 
 
 	//Movement//
-	public float PlayerGravity = 10f;
 	public float PlayerTurnSpeed = 10f;
 	public float PlayerMaxMoveSpeed = 5f;
 	public float PlayerAcceleration = 1f;
-	public bool IsInAir = false;
+	public float PlayerJumpStrenght = 10f;
+	public float PlayerFriction = 10f;
+
+
 	
 	private Vector3 PlayerPos;
 	private Vector3 PlayerNewRot;
 
-	protected Vector3 PlayerRot;
+
 	protected float PlayerSpeed;
+	protected bool PlayerIsJumping;
+	protected bool PlayerIsInAir;
+
+	protected Vector3 PlayerRot;
 	protected Vector3 PlayerVelDir;
 	protected Vector3 PlayerVel;
 		
 
-
+	CharacterController CharController;
 
 
  
 	// Use this for initialization
 	void Start () 
 	{
-	
+		CharController = GetComponent<CharacterController>();
+		Screen.lockCursor = true;
 	}
 	
 	// Update is called once per frame
@@ -43,17 +51,25 @@ public class PlayerController : MonoBehaviour {
 	
 	void HandleInput()
 	{
+		//Reset Vars
+		PlayerVelDir = new Vector3();
+		PlayerNewRot = new Vector3();
 		PlayerRot = transform.rotation.eulerAngles;
 		PlayerPos = transform.position;	
+
 		//Check player movement
-		if(Mathf.Abs(Input.GetAxis("LeftRight")) > 0 || Mathf.Abs(Input.GetAxis("ForwardBackward")) > 0  )
+		if(Mathf.Abs(Input.GetAxis("LeftRight")) > 0.1 || Mathf.Abs(Input.GetAxis("ForwardBackward")) > 0.1  )
 			PlayerVelDir = new Vector3(Input.GetAxis("LeftRight"),0,Input.GetAxis("ForwardBackward"));
 
-			if(PlayerSpeed != PlayerMaxMoveSpeed)
-				PlayerSpeed += PlayerAcceleration;
+				if(PlayerSpeed != PlayerMaxMoveSpeed)
+					PlayerSpeed += PlayerAcceleration;
 
-		if(Mathf.Abs(Input.GetAxis("Mouse X")) > 0 || Mathf.Abs(Input.GetAxis("Mouse Y")) > 0)
+		//Check Mouse Rotation 
+		if(Mathf.Abs(Input.GetAxis("Mouse X")) > 0.1 || Mathf.Abs(Input.GetAxis("Mouse Y")) > 0.1)
 			PlayerNewRot = new Vector3(Input.GetAxis("Mouse Y"),-Input.GetAxis("Mouse X"),0);
+
+		if(Input.GetButton("Jump") && CharController.isGrounded)		
+			PlayerIsJumping = true;
 
 
 
@@ -65,15 +81,30 @@ public class PlayerController : MonoBehaviour {
 
 		PlayerVel = PlayerVelDir  * PlayerSpeed ;
 
+		if(!CharController.isGrounded)	
+			PlayerVel.y += Physics.gravity.y;
+			PlayerIsInAir = true;
+
+		if(PlayerIsJumping)		
+			PlayerVel.y += PlayerJumpStrenght;
+			PlayerIsJumping = false;
+			
+
+
 		//DEBUG
 		Debug.DrawLine (PlayerPos, PlayerPos + PlayerVel , Color.red);	
-		Debug.Log(PlayerRot + "     " + PlayerNewRot);
+		Debug.Log("     " + PlayerVel.normalized + PlayerVel);
 
 
 
 		//Set Player Position
-		transform.position = PlayerPos + PlayerVel * Time.deltaTime;
-		transform.Rotate(PlayerNewRot * PlayerTurnSpeed);
+		CharController.Move( PlayerVel * Time.deltaTime);
+		//transform.position +=  PlayerVel * Time.deltaTime;
+		transform.Rotate(new Vector3(0,-PlayerNewRot.y * PlayerTurnSpeed,0));
+		Camera.main.transform.Rotate(new Vector3(-PlayerNewRot.x * PlayerTurnSpeed,0,0));
+
+	
+
 
 	}
 
